b9883a4b6f7e0ab37a010b57ffacfb9e16b35be1
norbat-dev <norbat@norbat.dev>
2025-12-02 01:26:03 +0100

    Task 1 Part 2

--- 01/index.php
+++ 01/index.php
@@ -4,6 +4,7 @@ class Dial
 {
     private const START = 50;
     private $current = 50;
+    public $zero_passes = 0;
     public $how_many_zeros = 0;
     private $file_handler;
 
@@ -31,11 +32,24 @@ class Dial
         }
     }
 
+    public function decrypt_part_two()
+    {
+        if( ! $this->file_handler )
+        {
+            return;
+        }
+
+        while ( $line = fgets( $this->file_handler ) )
+        {
+            $this->calc_pass_zeros( $line );
+        }
+    }
+
     private function rotate( $command )
     {
-        // echo "Current value: " . $this->current . ". Next step: " . $command;
         $direction = substr( $command, 0, 1 );
-        $step   = (int) substr( $command, 1 ) % 100;
+        $steps     = (int) substr( $command, 1 );
+        $step   = abs( $steps ) % 100;
         if( $direction == 'R' )
         {
             $this->current += $step;
@@ -54,10 +68,51 @@ class Dial
             $this->how_many_zeros++;
         }
     }
+
+    private function calc_pass_zeros( $command )
+    {
+        $direction = substr( $command, 0, 1 );
+        $steps     = (int) substr( $command, 1 );
+        $step   = abs( $steps ) % 100;
+        $full_rotation = floor( abs( $steps ) / 100 );
+
+        if( $direction == 'R' )
+        {
+            if( $this->current + $step > 100 AND $this->current + $step != 0 )
+            {
+                $this->zero_passes++;
+            }
+            $this->current = ( $this->current + $step ) % 100;
+        } else {
+            if( $this->current - $step < 0 AND $this->current != 0 )
+            {
+                $this->zero_passes++;
+                $this->current = 100 - ( $step - $this->current );
+            } else if( $this->current == 0) {
+                $this->current = 100 - ( $step - $this->current );
+            } else {
+                $this->current -= $step;
+            }
+        }
+        $this->current == 100 ? 0 : $this->current;
+        if( $this->current == 0 ){
+            $this->zero_passes++;
+        }
+
+        $this->zero_passes += $full_rotation;
+    }
+
+    public function reset_data()
+    {
+        $this->current = self::START;
+    }
 }
 
 
 $dial = new Dial;
 echo "Current value: 50" . PHP_EOL;
 $dial->load_input( 'input.txt' )->decrypt();
-echo $dial->how_many_zeros . PHP_EOL;
+echo "How many zeros: " . $dial->how_many_zeros . PHP_EOL;
+$dial->reset_data();
+$dial->load_input( 'input.txt' )->decrypt_part_two();
+echo "How many zero clicks: " . $dial->zero_passes . PHP_EOL;
