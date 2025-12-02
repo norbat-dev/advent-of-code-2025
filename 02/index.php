334f026a35c69bc7f48790c1aadc71a52419cfd6
norbat-dev <norbat@norbat.dev>
2025-12-02 23:31:49 +0100

    Task 2 Part 2

--- 02/index.php
+++ 02/index.php
@@ -48,6 +48,7 @@ class scanID
 
     public function sum_invalid()
     {
+        $this->invalid_ids = array_unique( $this->invalid_ids );
         foreach( $this->invalid_ids as $invalid_id ){
             $this->sum_invalid_ids += (int) $invalid_id;
         }
@@ -57,6 +58,30 @@ class scanID
         return $this->sum_invalid_ids;
     }
 
+    public function reset_sum()
+    {
+        $this->sum_invalid_ids = 0;
+        $this->invalid_ids = [];
+    }
+
+    function find_invalid_more_complex()
+    {
+        foreach( $this->ids as $id_range )
+        {
+            $first_id = (int) explode( '-', $id_range)[0];
+            $last_id = (int) explode( '-', $id_range)[1];
+            for( $current = $first_id; $current <= $last_id; $current++  ){
+                $pattern_max_lenght = floor( strlen( (string) $current ) / 2 );
+                for( $patter_length = 1; $patter_length  <= $pattern_max_lenght; $patter_length++)
+                {
+                    if( count( array_unique( str_split($current, $patter_length) ) ) === 1 )
+                    {
+                        $this->invalid_ids[] = $current;
+                    }
+                }
+            }
+        }
+    }
 
 }
 
@@ -66,6 +91,10 @@ $IDs = new scanID;
 $IDs->load_input( 'input.txt' )->read_ids_ranges();
 $IDs->find_invalid();
 $IDs->sum_invalid();
-echo 'Sum of invalid IDs is equal to ' . $IDs->get_sum_invalid_ids() .  PHP_EOL;
+echo '[Part 1] Sum of invalid IDs is equal to ' . $IDs->get_sum_invalid_ids() .  PHP_EOL;
+$IDs->reset_sum();
+$IDs->find_invalid_more_complex();
+$IDs->sum_invalid();
+echo '[Part 2] Sum of invalid IDs is equal to ' . $IDs->get_sum_invalid_ids() .  PHP_EOL;
 
 ?>
